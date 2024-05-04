cf3e09c27a2819257a4467e8780e1d0864e9ece5
Joby-John <joby432john@gmail.com>
2024-05-04 11:10:39 +0530

    learned till php variables

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo"This is a title";?></title>
+</head>
+<body >
+<?php
+echo "hello world\n";
+echo "This is me testing whether reload works";
+echo 1+2;
+
+?>
+
+    
+</body>
+</html>
+
---
+++ variable.php
@@ -0,0 +1,8 @@
+<?php
+ECHO "This is a script in which ill be learning php variables<br>";
+
+// variables start with $
+$name = "Joby";
+$salary = 1000000;
+echo "My name is $name with a salary of $salary<br>";
+?>
